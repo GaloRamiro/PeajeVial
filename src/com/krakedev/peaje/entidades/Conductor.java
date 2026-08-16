93e58c5f29196de78bd1dd78b77ccd951c4baeaa
GaloRamiro <g_cisneros_23@hotmail.es>
2026-08-15 21:03:41 -0500

    feat: agregar constructor de Conductor

--- src/com/krakedev/peaje/entidades/Conductor.java
+++ src/com/krakedev/peaje/entidades/Conductor.java
@@ -4,4 +4,12 @@ public class Conductor {
 	private String cedula;
 	private String nombre;
 	private String apellido;
+	
+	
+	
+	public Conductor(String cedula, String nombre, String apellido) {
+	    this.cedula = cedula;
+	    this.nombre = nombre;
+	    this.apellido = apellido;
+	}
 }
